aaf963455ec18d73238acec5b0090f02db024cfd
出帆 髙橋 <180179@jc-21.jp>
2020-12-04 09:38:13 +0900

    課題1,2,5,6,7,11,12,13,14　終了

---
+++ WebContent/WEB-INF/JSP/page2.6.jsp
@@ -0,0 +1,19 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+<h1>エラー</h1>
+その番号Aはすべての数字を足して９で割り切れません。
+
+<form method="get" action="./page1">
+
+<input type="submit" value="戻る" />
+</form>
+
+</body>
+</html>
--- src/Page2Servlet.java
+++ src/Page2Servlet.java
@@ -56,7 +56,14 @@ public class Page2Servlet extends HttpServlet {
 			rt.setString(1, sNumA);
 			rt.setString(2, sNumB);
 			
+			
 			ResultSet ss = rt.executeQuery();
+			
+			String NumAK[]= sNumA.split("");
+			int sum=0;
+			for(int i=0;i<7;i++) {
+				sum+=Integer.parseInt(NumAK[i]);
+			}
 			if(ss.next()==true) {
 				System.out.println("d•¡");
 				RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/JSP/page2.5.jsp");
@@ -66,20 +73,31 @@ public class Page2Servlet extends HttpServlet {
 			
 			
 			
+			
+			
 			else {
+				
+				if(sum%9!=0) {
+					System.out.println("NOT9");
+					RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/JSP/page2.6.jsp");
+					rd.forward(request, response);
+				}
+				
+				else {
 			
-			PreparedStatement st = 
-					connection.prepareStatement(
-							"Insert into OUBO Values(?,?,?)"
-						);
-			st.setString(1, sEname);
-			st.setString(2, sNumA);
-			st.setString(3, sNumB);
+					PreparedStatement st = 
+							connection.prepareStatement(
+									"Insert into OUBO Values(?,?,?,SYSDATE)"
+									);
+					st.setString(1, sEname);
+					st.setString(2, sNumA);
+					st.setString(3, sNumB);
 			
-			st.executeUpdate();
-			RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/JSP/page2.jsp");
-			rd.forward(request, response);
-			}
+					st.executeUpdate();
+					RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/JSP/page2.jsp");
+					rd.forward(request, response);
+				}
+				}
 		}catch(SQLException e) {
 			System.out.println("SQLException");
 			response.getWriter().println("SQLException");
